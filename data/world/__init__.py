9da96cf0fb4f1ccae1a5a5bd5ca9ce6e84f0f5e7
tea2code <stefan4875@gmail.com>
2014-01-28 17:41:07 +0100

    Removed caching of entities cause it is no longer supported. Closes #67.

--- data/world/__init__.py
+++ data/world/__init__.py
@@ -3,21 +3,7 @@ from .point import Factory as PointFactory
 from ..task import Factory as TaskFactory
 
 class Factory:
-    """ Factory class to create world entities.
-
-    Constants:
-    CACHE_STATICS  -- Indicates if static entities should be cached (bool).
-    CACHE_TILES -- Indicates if tiles should be cached (bool).
-
-    Member:
-    _statics -- Maps already created static entities (dict).
-    _tiles -- Maps already created tiles (dict).
-    """
-
-    CACHE_STATICS = False # TODO Currently not working. Entities disappear.
-    CACHE_TILES = False
-    _statics = {}
-    _tiles = {}
+    """ Factory class to create world entities."""
 
     @staticmethod
     def new_dynamic_entity(id, data, run_time, init_task=True):
@@ -49,16 +35,6 @@ class Factory:
     @staticmethod
     def new_static_entity(id, data):
         """ Creates and returns a new static entity. """
-        if Factory.CACHE_STATICS:
-            if id not in Factory._statics:
-                Factory._statics[id] = Factory.__new_static_entity(id, data)
-            return Factory._statics[id]
-        else:
-            return Factory.__new_static_entity(id, data)
-
-    @staticmethod
-    def __new_static_entity(id, data):
-        """ Creates and returns a new static entity. """
         config = data.config.entity.statics[id]
         entity = staticentity.StaticEntity(id)
         entity.blocked = [b.type for b in config.blocked]
@@ -79,15 +55,6 @@ class Factory:
     @staticmethod
     def new_tile(id, data):
         """ Creates and returns a new tile. """
-        if Factory.CACHE_TILES:
-            if id not in Factory._tiles:
-                Factory._tiles[id] = Factory.__new_tile(id, data)
-            return Factory._tiles[id]
-        else:
-            return Factory.__new_tile(id, data)
-
-    @staticmethod
-    def __new_tile(id, data):
         config = data.config.entity.tiles[id]
         entity = tile.Tile(id)
         entity.blocking = [b.type for b in config.blocking]
