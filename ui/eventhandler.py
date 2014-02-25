e01cb3a314e3b70463011cdc0fa3ea523ac621b6
tea2code <stefan4875@gmail.com>
2014-02-25 17:51:42 +0100

    Set cursor based on mouse input. Closes #136.

--- ui/eventhandler.py
+++ ui/eventhandler.py
@@ -3,6 +3,7 @@ import sys
 
 import pygame
 
+from . import helper
 from .keys import PygameKeys
 from data import id as ID
 from data.world import point
@@ -27,30 +28,25 @@ class PygameEventHandler:
                 logger.info('Run Time = {0:.2f}s; FPS = {1:.2f}'.format(run_time, sum(data.fps) / len(data.fps)))
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                self.__keydown(event, data)
+                self.__key_down(event, data)
+            elif event.type == pygame.MOUSEBUTTONDOWN:
+                self.__mouse_down(event, data)
 
-    def __keydown(self, event, data):
+    def __key_down(self, event, data):
+        """ Handle key down events. """
         config = data.config
         game = data.game
         graph = data.graphics
 
         # Cursor
         if event.key == self._keys[ID.ACTION_CURSOR_NORTH]:
-            data.dirty_pos.add(data.graphics.cursor)
-            data.graphics.cursor += point.Factory.new_point(0, -1, 0)
-            data.dirty_pos.add(data.graphics.cursor)
+            self.__move_cursor(data, point.Factory.new_point(0, -1, 0))
         elif event.key == self._keys[ID.ACTION_CURSOR_SOUTH]:
-            data.dirty_pos.add(data.graphics.cursor)
-            data.graphics.cursor += point.Factory.new_point(0, +1, 0)
-            data.dirty_pos.add(data.graphics.cursor)
+            self.__move_cursor(data, point.Factory.new_point(0, +1, 0))
         elif event.key == self._keys[ID.ACTION_CURSOR_WEST]:
-            data.dirty_pos.add(data.graphics.cursor)
-            data.graphics.cursor += point.Factory.new_point(-1, 0, 0)
-            data.dirty_pos.add(data.graphics.cursor)
+            self.__move_cursor(data, point.Factory.new_point(-1, 0, 0))
         elif event.key == self._keys[ID.ACTION_CURSOR_EAST]:
-            data.dirty_pos.add(data.graphics.cursor)
-            data.graphics.cursor += point.Factory.new_point(+1, 0, 0)
-            data.dirty_pos.add(data.graphics.cursor)
+            self.__move_cursor(data, point.Factory.new_point(+1, 0, 0))
 
         # Scrolling
         elif event.key == self._keys[ID.ACTION_SCROLL_NORTH]:
@@ -69,3 +65,21 @@ class PygameEventHandler:
             graph.view_offset_x = min(graph.view_offset_x + config.scroll_width_x,
                                       game.size_x - graph.view_x)
             graph.view_updated = True
+
+    def __mouse_down(self, event, data):
+        """ Handle mouse down events. """
+        graphics = data.graphics
+        screen_pos = pygame.mouse.get_pos()
+        x, y = helper.screen_to_pos(point.Factory.new_point(screen_pos[0], screen_pos[1], 0),
+                                    graphics.tile_x, graphics.tile_y)
+        data.dirty_pos.add(graphics.cursor)
+        graphics.cursor = point.Factory.new_point(x + graphics.view_offset_x,
+                                                  y + graphics.view_offset_y,
+                                                  0)
+        data.dirty_pos.add(graphics.cursor)
+
+    def __move_cursor(self, data, direction):
+        """ Moves cursor in direction. """
+        data.dirty_pos.add(data.graphics.cursor)
+        data.graphics.cursor += direction
+        data.dirty_pos.add(data.graphics.cursor)
--- ui/helper.py
+++ ui/helper.py
@@ -1,5 +1,6 @@
 """ Some ui related helper functions. """
 
+
 def is_visible(pos, graphics):
     """ Checks if a position is visible on screen.
 
@@ -33,6 +34,7 @@ def is_visible(pos, graphics):
     return graphics.view_offset_x <= pos.x < graphics.view_offset_x + graphics.view_x and \
            graphics.view_offset_y <= pos.y < graphics.view_offset_y + graphics.view_y
 
+
 def pos_to_screen(pos, tile_x, tile_y):
     """ Convert world coordinates to tile position on screen. This
     returns the left upper corner.
@@ -47,6 +49,23 @@ def pos_to_screen(pos, tile_x, tile_y):
     (16, 36)
     """
     return pos.x * tile_x, pos.y * tile_y
+
+
+def screen_to_pos(pos, tile_x, tile_y):
+    """ Convert world coordinates to tile position on screen. This
+    returns the left upper corner.
+
+    Test:
+    >>> from data.world import point
+    >>> p = point.Point(0, 0)
+    >>> screen_to_pos(p, 16, 18)
+    (0, 0)
+    >>> p = point.Point(16, 36)
+    >>> screen_to_pos(p, 16, 18)
+    (1, 2)
+    """
+    return int(pos.x / tile_x), int(pos.y / tile_y)
+
 
 if __name__ == '__main__':
     print('Executing doctest.')
