c39615f6cc9f764d37159fbcb4916455ee8acf69
tea2code <stefan4875@gmail.com>
2014-03-23 13:10:10 +0100

    Use new get list method of configuration to read load file.

--- src/Application.cpp
+++ src/Application.cpp
@@ -14,41 +14,10 @@ frts::Application::LoadConfiguration frts::Application::readLoadFile(const std::
     ConfigNodePtr node = parser.parseFile(filePath);
 
     LoadConfiguration result;
-
-    // Plugins
-    ConfigNodePtr subNode = node->getNode("plugins");
-    for (const auto& valueNode : *subNode)
-    {
-        result.plugins.push_back(valueNode->getString(""));
-    }
-
-    // Render modules
-    subNode = node->getNode("renderModules");
-    for (const auto& valueNode : *subNode)
-    {
-        result.renderModules.push_back(valueNode->getString(""));
-    }
-
-    // Update modules
-    subNode = node->getNode("updateModules");
-    for (const auto& valueNode : *subNode)
-    {
-        result.updateModules.push_back(valueNode->getString(""));
-    }
-
-    // Utilities
-    subNode = node->getNode("utilities");
-    for (const auto& valueNode : *subNode)
-    {
-        result.utilities.push_back(valueNode->getString(""));
-    }
-
-    // Configurations
-    subNode = node->getNode("configurations");
-    for (const auto& valueNode : *subNode)
-    {
-        result.configurations.push_back(valueNode->getString(""));
-    }
-
+    result.plugins = node->getStrings("plugins");
+    result.renderModules = node->getStrings("renderModules");
+    result.updateModules = node->getStrings("updateModules");
+    result.utilities = node->getStrings("utilities");
+    result.configurations = node->getStrings("configurations");
     return result;
 }
