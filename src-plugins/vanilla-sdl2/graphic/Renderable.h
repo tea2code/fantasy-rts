94a8ac664af939b9faf78a1d44fd625e32aff201
tea2code <stefan4875@gmail.com>
2014-07-13 09:51:55 +0200

    Commited autosave.

--- src-plugins/vanilla-sdl2/graphic/Renderable.h
+++ src-plugins/vanilla-sdl2/graphic/Renderable.h
@@ -1,4 +1,53 @@
-#ifndef RENDERABLE_H
-#define RENDERABLE_H
+#ifndef FRTS_RENDERABLE_H
+#define FRTS_RENDERABLE_H
 
-#endif // RENDERABLE_H
+#include <frts/vanillamodel>
+
+#include <memory>
+
+
+namespace frts
+{
+    class Renderable;
+
+    /**
+     * @brief Pointer to Renderable.
+     */
+    using RenderablePtr = std::shared_ptr<Renderable>;
+
+    /**
+     * @brief Component describing if a entity is renderable and specifying how.
+     */
+    class Renderable : public Component
+    {
+    public:
+        virtual void ~Renderable() {}
+
+        /**
+         * @brief Get the ID of the sprite to use.
+         * @return The ID.
+         */
+        virtual IdPtr getSprite() const = 0;
+
+        /**
+         * @brief Get the index of the sprite. Default 0 but may change if sprite
+         *        has multiple attribute.
+         * @return The index.
+         */
+        virtual int getSpriteIndex() const = 0;
+
+        /**
+         * @brief Set the ID of the sprite to use.
+         * @param id The ID.
+         */
+        virtual void setSprite(IdPtr id) = 0;
+
+        /**
+         * @brief Set the index of the sprite.
+         * @param index The index.
+         */
+        virtual void setSpriteIndex(int index) = 0;
+    };
+}
+
+#endif // FRTS_RENDERABLE_H
